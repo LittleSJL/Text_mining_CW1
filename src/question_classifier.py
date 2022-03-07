50aae88e1ec2f9d4de57c37deae578195003dc5a
Jinliang <851788212@qq.com>
2022-03-07 14:50:35 +0000

    version 5.0 (finish testing)

--- src/question_classifier.py
+++ src/question_classifier.py
@@ -4,7 +4,7 @@ from input_handler import load_config_file, InputHandler
 
 
 # read configuration file
-config_path = "../data/bilstm_glove_finetune.ini"
+config_path = "../data/bow_glove_finetune.ini"
 config = load_config_file(config_path)
 # build input_handler
 input_handler = InputHandler(config)
@@ -25,6 +25,12 @@ if is_training:
                          embedding_matrix=input_handler.get_embedding_matrix())
     model.train(train_data, train_label, dev_data, dev_label)
 else:
+    # load and pre-process the training data and dev data for training
+    x_train, x_dev, y_train, y_dev = input_handler.get_training_data()
+    # transform text data and label into tensor vectors
+    train_data, train_label = input_handler.transform_input(sentences=x_train, labels=y_train)
+    dev_data, dev_label = input_handler.transform_input(sentences=x_dev, labels=y_dev)
+
     # load and pre-process the test data for testing
     raw_x_test, x_test, y_test = input_handler.get_test_data()
     # transform text data and label into tensor vectors
